393b4d366c42edf854cebc8fa1c721bb2dd0a921
Dave Love <d.love@liverpool.ac.uk>
2016-07-25 15:54:11 +0100

    Add .dir-locals.el for Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;; Minimal attempt for Singularity style
+((nil
+  . ((indent-tabs-mode . nil)))
+ (c-mode
+  . ((c-file-style . "bsd")             ; ?
+     (c-basic-offset . 4)
+     (parens-require-spaces . nil)      ; not safe-local by default
+     )))
